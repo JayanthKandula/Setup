e8bfa02675bfd959620be5fd581052de29557858
JayanthK <71193355+JayanthKandula@users.noreply.github.com>
2024-02-01 12:14:10 +0530

    Create Remove.sh

---
+++ Remove.sh
@@ -0,0 +1,7 @@
+rm -rf ~/los/packages/apps/Trebuchet
+rm -rf ~/los/packages/apps/Updater
+rm -rf ~/los/packages/apps/Settings
+rm -rf ~/los/system/sepolicy
+rm -rf ~/los/device/lineage/sepolicy
+rm -rf ~/los/system/core
+rm -rf ~/los/frameworks/base
