ae4029b4ccbcea7914aa69fb155039adfc42974f
Jayanth Kandula <k.jayanth94@gmail.com>
2024-02-04 14:37:42 +0530

    Update setup script

--- setup.sh
+++ setup.sh
@@ -8,9 +8,6 @@ cd ~/los/packages/apps/Settings
 git fetch  https://github.com/JayanthKandula/android_packages_apps_Settings lineage-21.0
 git cherry-pick 8592bec6f137c4eefe209ad20b1c91bcdf7cbd7d
 git cherry-pick 8592bec6f137c4eefe209ad20b1c91bcdf7cbd7d..8fd01b889f6573ebdc63c6b2e5fbf78a8a75b37b
-cd ~/los/device/lineage/sepolicy
-git fetch  https://github.com/JayanthKandula/android_device_lineage_sepolicy lineage-21.0
-git cherry-pick 653aed8f11efed8e003dc5b7f7fbf6a3697df77e
 cd ~/los/system/core
 git fetch  https://github.com/JayanthKandula/android_system_core lineage-21.0
 git cherry-pick d988ab03352fcd72e97b3f85d42425ef6bfd1e82
@@ -19,6 +16,12 @@ cd ~/los/frameworks/base
 git fetch  https://github.com/JayanthKandula/android_frameworks_base lineage-21.0
 git cherry-pick 1e50adf4bb3ba09f9ca89fb6d512858a66759e90
 git cherry-pick 1e50adf4bb3ba09f9ca89fb6d512858a66759e90..e66944d9ee65b8fe89aa8e2f2af9e4044b29880a
+cd ~/los/vendor/lineage
+git fetch https://github.com/JayanthKandula/android_vendor_lineage lineage-21.0
+git cherry-pick ac92c0ad448bab63ede53316a88e0c78f47d4aa6
+cd ~/los/packages/overlays/Lineage
+git fetch https://github.com/JayanthKandula/android_packages_overlays_Lineage.git lineage-21.0
+git cherry-pick b96670d4c93059c2e75796d91f3274536c021b98
 cd ~/los
 git clone -b lineage-21 https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
 cd ~/los
