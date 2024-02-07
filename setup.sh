d22f27879db8cdb3914229b48e369d09a38625dc
Jayanth Kandula <k.jayanth94@gmail.com>
2024-02-07 22:10:07 +0530

    Update script

--- setup.sh
+++ setup.sh
@@ -6,8 +6,8 @@ git fetch  https://github.com/JayanthKandula/android_packages_apps_Updater linea
 git cherry-pick 86cc1276049e1ea92f3592a1f84d5d83238ff94f
 cd ~/los/packages/apps/Settings
 git fetch  https://github.com/JayanthKandula/android_packages_apps_Settings lineage-21.0
-git cherry-pick 8592bec6f137c4eefe209ad20b1c91bcdf7cbd7d
-git cherry-pick 8592bec6f137c4eefe209ad20b1c91bcdf7cbd7d..8fd01b889f6573ebdc63c6b2e5fbf78a8a75b37b
+git cherry-pick 5874c61eb4ad35efeae6d0832603a72c8921e485
+git cherry-pick 5874c61eb4ad35efeae6d0832603a72c8921e485..ae67830bc4de2a3b2bf5046b1c0ae536056f6643
 cd ~/los/system/core
 git fetch  https://github.com/JayanthKandula/android_system_core lineage-21.0
 git cherry-pick d988ab03352fcd72e97b3f85d42425ef6bfd1e82
@@ -19,6 +19,7 @@ git cherry-pick 1e50adf4bb3ba09f9ca89fb6d512858a66759e90..e66944d9ee65b8fe89aa8e
 cd ~/los/vendor/lineage
 git fetch https://github.com/JayanthKandula/android_vendor_lineage lineage-21.0
 git cherry-pick ac92c0ad448bab63ede53316a88e0c78f47d4aa6
+git cherry-pick ac92c0ad448bab63ede53316a88e0c78f47d4aa6..edb5126e70be9ba149f5d8fadb1d5cb5351f641a
 cd ~/los/packages/overlays/Lineage
 git fetch https://github.com/JayanthKandula/android_packages_overlays_Lineage.git lineage-21.0
 git cherry-pick b96670d4c93059c2e75796d91f3274536c021b98
