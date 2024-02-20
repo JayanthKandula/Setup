5c6742e56df8380d357e8277390e2cdb11389505
Jayanth Kandula <k.jayanth94@gmail.com>
2024-02-20 20:34:50 +0530

    update

--- setup.sh
+++ setup.sh
@@ -1,13 +1,7 @@
-cd ~/los/packages/apps/Trebuchet
-git fetch  https://github.com/JayanthKandula/android_packages_apps_Trebuchet lineage-21.0
-git cherry-pick bab49a12b3efe2d0435157d44fe36f5f631f3cd1
-cd ~/los/packages/apps/Updater
-git fetch  https://github.com/JayanthKandula/android_packages_apps_Updater lineage-21.0
-git cherry-pick 86cc1276049e1ea92f3592a1f84d5d83238ff94f
 cd ~/los/packages/apps/Settings
 git fetch  https://github.com/JayanthKandula/android_packages_apps_Settings lineage-21.0
 git cherry-pick 5874c61eb4ad35efeae6d0832603a72c8921e485
-git cherry-pick 5874c61eb4ad35efeae6d0832603a72c8921e485..ae67830bc4de2a3b2bf5046b1c0ae536056f6643
+git cherry-pick 5874c61eb4ad35efeae6d0832603a72c8921e485..32c193f20c373185f70afdb775842fb436a6ddfc
 cd ~/los/system/core
 git fetch  https://github.com/JayanthKandula/android_system_core lineage-21.0
 git cherry-pick d988ab03352fcd72e97b3f85d42425ef6bfd1e82
@@ -15,7 +9,9 @@ git cherry-pick d988ab03352fcd72e97b3f85d42425ef6bfd1e82..d890a2fe0ab0e8c92ec86f
 cd ~/los/frameworks/base
 git fetch  https://github.com/JayanthKandula/android_frameworks_base lineage-21.0
 git cherry-pick 1e50adf4bb3ba09f9ca89fb6d512858a66759e90
-git cherry-pick 1e50adf4bb3ba09f9ca89fb6d512858a66759e90..62631e2dae15164965ff21d5e6fa82b630a07eaf
+git cherry-pick 1e50adf4bb3ba09f9ca89fb6d512858a66759e90..e61a1b32204fb4b1be74dd114ad631dd3fe95c55
+git cherry-pick 33439f68fcfb9fccba790dc48b21c69b0f2e8a23
+git cherry-pick 33439f68fcfb9fccba790dc48b21c69b0f2e8a23..a81942e56fa6a54c600c47cad6f697021e9b126e
 cd ~/los/vendor/lineage
 git fetch https://github.com/JayanthKandula/android_vendor_lineage lineage-21.0
 git cherry-pick ac92c0ad448bab63ede53316a88e0c78f47d4aa6
@@ -24,5 +20,3 @@ cd ~/los/packages/overlays/Lineage
 git fetch https://github.com/JayanthKandula/android_packages_overlays_Lineage.git lineage-21.0
 git cherry-pick b96670d4c93059c2e75796d91f3274536c021b98
 git cherry-pick b96670d4c93059c2e75796d91f3274536c021b98..6cfb2bf3b2d44f09cce378c65517bbf690bb4716
-cd ~/los
-git clone -b lineage-21 https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
