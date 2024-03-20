71f15e4b52517ee108b7e78fd1f311685b1282cf
Jayanth Kandula <k.jayanth94@gmail.com>
2024-03-20 15:25:48 +0530

    update

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@ git fetch  https://github.com/JayanthKandula/android_frameworks_base lineage-21.
 git cherry-pick 1e50adf4bb3ba09f9ca89fb6d512858a66759e90
 git cherry-pick 1e50adf4bb3ba09f9ca89fb6d512858a66759e90..e61a1b32204fb4b1be74dd114ad631dd3fe95c55
 git cherry-pick 62631e2dae15164965ff21d5e6fa82b630a07eaf
-git cherry-pick 62631e2dae15164965ff21d5e6fa82b630a07eaf..a81942e56fa6a54c600c47cad6f697021e9b126e
+git cherry-pick 62631e2dae15164965ff21d5e6fa82b630a07eaf..0bdb6558d0335f60c5fe8b19b487923dd4f3096b
 cd ~/los/vendor/lineage
 git fetch https://github.com/JayanthKandula/android_vendor_lineage lineage-21.0
 git cherry-pick ac92c0ad448bab63ede53316a88e0c78f47d4aa6
@@ -22,8 +22,8 @@ git cherry-pick b96670d4c93059c2e75796d91f3274536c021b98
 git cherry-pick b96670d4c93059c2e75796d91f3274536c021b98..6cfb2bf3b2d44f09cce378c65517bbf690bb4716
 cd ~/los/packages/apps/Trebuchet
 git fetch https://github.com/JayanthKandula/android_packages_apps_Trebuchet.git lineage-21.0
-git cherry-pick 1d4480a492ef47f66de88169c36480aea25192c9
-git cherry-pick 1d4480a492ef47f66de88169c36480aea25192c9..3f5d952e6403b98334637b47ba94cefc12134616
+git cherry-pick 17c95a8cd82035555892d32f3e04344447cd108c
+git cherry-pick 17c95a8cd82035555892d32f3e04344447cd108c..9b7544b0489c8541f1a4d235a2aa7bacb2c38f33
 cd ~/los/packages/apps/Updater
 git fetch https://github.com/JayanthKandula/android_packages_apps_Updater lineage-21.0
 git cherry-pick 86cc1276049e1ea92f3592a1f84d5d83238ff94f
