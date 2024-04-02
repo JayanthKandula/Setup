e8ad56b0fe594eeb5ba97cce739bd2750bade590
Jayanth Kandula <k.jayanth94@gmail.com>
2024-04-02 20:18:41 +0530

    Update

--- setup.sh
+++ setup.sh
@@ -1,29 +1,31 @@
-cd ~/los/packages/apps/Settings
-git fetch  https://github.com/JayanthKandula/android_packages_apps_Settings lineage-21.0
-git cherry-pick 5874c61eb4ad35efeae6d0832603a72c8921e485
-git cherry-pick 5874c61eb4ad35efeae6d0832603a72c8921e485..32c193f20c373185f70afdb775842fb436a6ddfc
-cd ~/los/system/core
-git fetch  https://github.com/JayanthKandula/android_system_core lineage-21.0
-git cherry-pick d988ab03352fcd72e97b3f85d42425ef6bfd1e82
-git cherry-pick d988ab03352fcd72e97b3f85d42425ef6bfd1e82..d890a2fe0ab0e8c92ec86ff32d97ac069e30bf9f
 cd ~/los/frameworks/base
-git fetch  https://github.com/JayanthKandula/android_frameworks_base lineage-21.0
-git cherry-pick 1e50adf4bb3ba09f9ca89fb6d512858a66759e90
-git cherry-pick 1e50adf4bb3ba09f9ca89fb6d512858a66759e90..e61a1b32204fb4b1be74dd114ad631dd3fe95c55
-git cherry-pick 62631e2dae15164965ff21d5e6fa82b630a07eaf
-git cherry-pick 62631e2dae15164965ff21d5e6fa82b630a07eaf..0bdb6558d0335f60c5fe8b19b487923dd4f3096b
+git fetch git@github.com:JayanthKandula/android_frameworks_base.git lineage-21.0
+git cherry-pick 8d7b25a258ba79ee9b31bbd6c83204ed8ebc0660
+git cherry-pick 8d7b25a258ba79ee9b31bbd6c83204ed8ebc0660..0e2d7f020930fcafcc501309422f00e571083d3b
+
+cd ~/los/packages/apps/Settings
+git fetch git@github.com:JayanthKandula/android_packages_apps_Settings.git lineage-21.0
+git cherry-pick e8f9dd127e60e0dbb7bbbc14a25739b00e80b89a
+git cherry-pick e8f9dd127e60e0dbb7bbbc14a25739b00e80b89a..0b9972bbda1ef8515fe7e9b18e2774acc3561fc6
+
+cd ~/los/system/core
+git fetch git@github.com:JayanthKandula/android_system_core.git lineage-21.0
+git cherry-pick 359f8301e17aa16992d0d2c495b9fd7bf595fa57
+git cherry-pick 359f8301e17aa16992d0d2c495b9fd7bf595fa57..e018d4ce2ddfd65652ce9565571145a1650fc1e6
+
 cd ~/los/vendor/lineage
-git fetch https://github.com/JayanthKandula/android_vendor_lineage lineage-21.0
+git fetch git@github.com:JayanthKandula/android_vendor_lineage.git lineage-21.0
 git cherry-pick ac92c0ad448bab63ede53316a88e0c78f47d4aa6
-git cherry-pick ac92c0ad448bab63ede53316a88e0c78f47d4aa6..99399ab8f8f73d1b53ae5c3b2afe597281e4fc13
+
 cd ~/los/packages/overlays/Lineage
-git fetch https://github.com/JayanthKandula/android_packages_overlays_Lineage.git lineage-21.0
+git fetch git@github.com:JayanthKandula/android_packages_overlays_Lineage.git lineage-21.0
 git cherry-pick b96670d4c93059c2e75796d91f3274536c021b98
-git cherry-pick b96670d4c93059c2e75796d91f3274536c021b98..6cfb2bf3b2d44f09cce378c65517bbf690bb4716
+
 cd ~/los/packages/apps/Trebuchet
-git fetch https://github.com/JayanthKandula/android_packages_apps_Trebuchet.git lineage-21.0
-git cherry-pick 17c95a8cd82035555892d32f3e04344447cd108c
-git cherry-pick 17c95a8cd82035555892d32f3e04344447cd108c..9b7544b0489c8541f1a4d235a2aa7bacb2c38f33
+git fetch git@github.com:JayanthKandula/android_packages_apps_Trebuchet.git lineage-21.0
+git cherry-pick 2d8a6a2afa50c0dbfa42c960324511e4c4ff1269
+git cherry-pick 2d8a6a2afa50c0dbfa42c960324511e4c4ff1269..7b4fa8d66eaba1c6c2df1ae50dc294651e6a2e2d
+
 cd ~/los/packages/apps/Updater
 git fetch https://github.com/JayanthKandula/android_packages_apps_Updater lineage-21.0
 git cherry-pick 86cc1276049e1ea92f3592a1f84d5d83238ff94f
